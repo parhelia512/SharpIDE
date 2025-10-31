0c2a69ba990fef6fa42c51f848813bef63940a1f
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-10-31 21:44:52 +1000

    nuget - replace with custom entry

--- src/SharpIDE.Godot/Features/Nuget/NugetPanel.cs
+++ src/SharpIDE.Godot/Features/Nuget/NugetPanel.cs
@@ -4,4 +4,14 @@ namespace SharpIDE.Godot.Features.Nuget;
 
 public partial class NugetPanel : Control
 {
+    private VBoxContainer _installedPackagesVboxContainer = null!;
+    private VBoxContainer _implicitlyInstalledPackagesItemList = null!;
+    private VBoxContainer _availablePackagesItemList = null!;
+
+    public override void _Ready()
+    {
+        _installedPackagesVboxContainer = GetNode<VBoxContainer>("%InstalledPackagesVBoxContainer");
+        _implicitlyInstalledPackagesItemList = GetNode<VBoxContainer>("%ImplicitlyInstalledPackagesVBoxContainer");
+        _availablePackagesItemList = GetNode<VBoxContainer>("%AvailablePackagesVBoxContainer");
+    }
 }
