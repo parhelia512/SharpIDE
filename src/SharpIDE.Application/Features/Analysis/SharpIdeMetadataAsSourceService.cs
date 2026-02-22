390c6175b69e389f4f0c2fd446a830d42d0ed4b3
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2026-02-22 13:58:29 +1000

    cache decompiled SharpIdeFile

--- src/SharpIDE.Application/Features/Analysis/SharpIdeMetadataAsSourceService.cs
+++ src/SharpIDE.Application/Features/Analysis/SharpIdeMetadataAsSourceService.cs
@@ -6,12 +6,16 @@ namespace SharpIDE.Application.Features.Analysis;
 public class SharpIdeMetadataAsSourceService(RoslynAnalysis roslynAnalysis)
 {
 	private readonly RoslynAnalysis _roslynAnalysis = roslynAnalysis;
+	private readonly Dictionary<string, SharpIdeFile> _metadataAsSourceFileCache = [];
 
 	public async Task<SharpIdeFile?> CreateSharpIdeFileForMetadataAsSourceAsync(SharpIdeFile currentFile, ISymbol referencedSymbol)
 	{
 		var filePath = await _roslynAnalysis.GetMetadataAsSource(currentFile, referencedSymbol);
 		if (filePath is null) return null;
+		var fileFromCache = _metadataAsSourceFileCache.GetValueOrDefault(filePath);
+		if (fileFromCache is not null) return fileFromCache;
 		var metadataAsSourceSharpIdeFile = new SharpIdeFile(filePath, Path.GetFileName(filePath), Path.GetExtension(filePath), null!, [], true);
+		_metadataAsSourceFileCache[filePath] = metadataAsSourceSharpIdeFile;
 		return metadataAsSourceSharpIdeFile;
 	}
 }
